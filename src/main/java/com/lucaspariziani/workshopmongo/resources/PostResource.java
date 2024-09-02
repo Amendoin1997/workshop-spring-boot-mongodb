c1f4c06ca5b036e9b6df4318d0f51dbb2dcff8db
Amendoin1997 <lucaspariziani@gmail.com>
2024-09-02 13:54:32 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lucaspariziani/workshopmongo/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.lucaspariziani.workshopmongo.resources;
+
+import com.lucaspariziani.workshopmongo.domain.Post;
+import com.lucaspariziani.workshopmongo.domain.User;
+import com.lucaspariziani.workshopmongo.dto.UserDTO;
+import com.lucaspariziani.workshopmongo.services.PostService;
+import com.lucaspariziani.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/lucaspariziani/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.lucaspariziani.workshopmongo.services;
+
+import com.lucaspariziani.workshopmongo.domain.Post;
+import com.lucaspariziani.workshopmongo.domain.User;
+import com.lucaspariziani.workshopmongo.dto.UserDTO;
+import com.lucaspariziani.workshopmongo.repository.PostRepository;
+import com.lucaspariziani.workshopmongo.repository.UserRepository;
+import com.lucaspariziani.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+
+    @Autowired
+    private PostRepository repo;
+
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")) ;
+    }
+}

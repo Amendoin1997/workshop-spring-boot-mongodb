041b796b56f1550bf50af03b9cca8b324d238736
Amendoin1997 <lucaspariziani@gmail.com>
2024-09-02 03:31:09 -0300

    Obtendo um usuário por id

--- src/main/java/com/lucaspariziani/workshopmongo/services/UserService.java
+++ src/main/java/com/lucaspariziani/workshopmongo/services/UserService.java
@@ -2,18 +2,30 @@ package com.lucaspariziani.workshopmongo.services;
 
 import com.lucaspariziani.workshopmongo.domain.User;
 import com.lucaspariziani.workshopmongo.repository.UserRepository;
+import com.lucaspariziani.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+
 
     @Autowired
     private UserRepository repo;
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")) ;
+
+
     }
 }
